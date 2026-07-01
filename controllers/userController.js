f1e189b9f6e233f514e4026ecfd9136c5c880475
nitishchahal <nitishchoudharyc2@gmail.com>
2026-07-01 22:31:22 +0530

    Remove Razorpay integration

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,14 +6,14 @@ import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
 import { v2 as cloudinary } from 'cloudinary'
 import stripe from "stripe";
-import razorpay from 'razorpay';
+// import razorpay from 'razorpay';
 
 // Gateway Initialize
 const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY)
-const razorpayInstance = new razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
-})
+// const razorpayInstance = new razorpay({
+//     key_id: process.env.RAZORPAY_KEY_ID,
+//     key_secret: process.env.RAZORPAY_KEY_SECRET,
+// })
 
 // API to register user
 const registerUser = async (req, res) => {
@@ -351,8 +351,8 @@ export {
     bookAppointment,
     listAppointment,
     cancelAppointment,
-    paymentRazorpay,
-    verifyRazorpay,
+    // paymentRazorpay,
+    // verifyRazorpay,
     paymentStripe,
     verifyStripe
 }
