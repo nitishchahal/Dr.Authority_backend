9091dea1e3a983326d0905b28a8e7e467029c6f4
nitishchahal <nitishchoudharyc2@gmail.com>
2026-07-01 22:22:45 +0530

    config Stripe change

--- config/stripe.js
+++ config/stripe.js
@@ -1,5 +1,10 @@
 import Stripe from "stripe";
 
+console.log(
+  "Stripe key prefix:",
+  process.env.STRIPE_SECRET_KEY?.slice(0, 12)
+);
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default stripe;
